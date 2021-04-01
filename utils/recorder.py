8774229605c8ec6b8427368d9dc9e1bfad278c79
kshmawj111 <45940359+kshmawj111@users.noreply.github.com>
2021-04-01 14:49:20 +0900

    class Recorder implemented v1.1 Updated to fit with pandas well. Now returns Dict type history

--- utils/recorder.py
+++ utils/recorder.py
@@ -1,35 +1,56 @@
 """
     Class for recording num_iterations, execution time etc.
 
-    Designed to return python list
+    call by "execute" method with argument with F: function and args.
+
+    returns Dict type which contains the history
 
 """
 from time import time, time_ns
 from sorting.quick_sort import quick_sort
+import pandas as pd
 
 
 class Recorder(object):
     def __init__(self):
-        self.exe_nano = []
-        self.exe_sec = []
+        self.record = {}
 
+    # method for executing a specific function given as F
     def execute(self, F, *args, **kwargs):
+        self.initialize_F_info(F)
+
         s = time()
         s_n = time_ns()
         F(*args, **kwargs)
         end = time()
         end_n = time_ns()
 
-        self.exe_sec.append(end-s)
-        self.exe_nano.append(end_n-s_n)
+        self.record[F.__name__]['sec'].append(end-s)
+        self.record[F.__name__]['nano'].append(end_n - s_n)
 
+    # getter
     def get_times(self):
-        return self.exe_sec, self.exe_nano
+        return self.record
 
+    # add function info to member dictionary containing execution times
+    def initialize_F_info(self, F):
+        fun_name = F.__name__
 
+        if fun_name not in self.record.keys():
+            self.record[fun_name] = {'nano': [], 'sec': []}
+
+"""
+
+    Guide for using recorder object
+    
+"""
 if __name__ == '__main__':
     a = [5,8,4,6,2,3,1,1,2,5,6,8,6,4,5,8,9,6,2,1,3,2,5,6,2,3,4,5,4,6,4]
     recorder = Recorder()
     args = [a, 0, len(a)-1]
     recorder.execute(quick_sort, *args)
-    print(recorder.get_times())
+    record = recorder.get_times()
+
+    print(record)
+    df = pd.DataFrame.from_dict(record, orient='index')
+    print(df)
